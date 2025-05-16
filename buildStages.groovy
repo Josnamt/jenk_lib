f4ffccbc0c3e7ef54efab800bfa74bbca8e6004a
Josna Mary Thomas <josna.thomas@prevalent.ai>
2025-05-16 13:55:18 +0530

    new file added

---
+++ buildStages.groovy
@@ -0,0 +1,23 @@
+def buildAndTest() {
+    echo "Running Maven build and tests..."
+    sh 'mvn clean install'
+}
+
+def publishTestResults() {
+    echo "Publishing JUnit test results..."
+    junit '**/target/surefire-reports/*.xml'
+}
+
+def publishArtifacts() {
+    echo "Publishing artifacts to Artifactory..."
+
+    def server = Artifactory.server(env.ARTIFACTORY_SERVER)
+    def uploadSpec = """{
+        "files": [{
+            "pattern": "target/*.jar",
+            "target": "${env.ARTIFACTORY_REPO}/"
+        }]
+    }"""
+
+    server.upload spec: uploadSpec
+}
